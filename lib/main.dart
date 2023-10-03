47d8d155dc968c6c34112a8a8387802d8d4a018f
diak <diak@mesterlevel.lo>
2023-10-03 11:31:04 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:topjoy/views/home_view.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomeView(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
